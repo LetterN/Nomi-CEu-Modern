572cc8915ae5162cd7bb53a05b03d64ce0e6ee7f
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-10 11:17:12 +0200

    Added Micro Miners

---
+++ kubejs/startup_scripts/itemRegistry.js
@@ -0,0 +1,15 @@
+//Microminers
+StartupEvents.registry('item', event => {
+
+    event.create('microminer_t1').displayName("\u00A7eSteel Plated Micro Miner [Tier One]").maxStackSize(16)
+    event.create('microminer_t2').displayName("\u00A7eTitanium Plated Micro Miner [Tier Two]").maxStackSize(16)
+    event.create('microminer_t3').displayName("\u00A7eTungsten Carbide Plated Micro Miner [Tier Three]").maxStackSize(16)
+    event.create('microminer_t4').displayName("\u00A7eSignalum Plated Micro Miner [Tier Four]").maxStackSize(16)
+    event.create('microminer_t5').displayName("\u00A7bIridium Plated Micro Miner [Tier Five]").maxStackSize(16)
+    event.create('microminer_t6').displayName("\u00A7bEnderium Plated Micro Miner [Tier Six]").maxStackSize(16)
+    event.create('microminer_t7').displayName("\u00A7bDraconium Plated Micro Dragon Hunter [Tier Seven]").maxStackSize(16)
+    event.create('microminer_t8').displayName("\u00A7dCrystal Matrix Plated Micro Leviathan Slayer [Tier Eight]").maxStackSize(16)
+    event.create('microminer_t9').displayName("\u00A7dEternium Plated Micro Sun Eater [Tier Nine]").maxStackSize(16)
+    event.create('microminer_t10').displayName("\u00A7dNeutronium Plated Micro Universe Harvester [Tier Ten]").maxStackSize(16)
+
+})
