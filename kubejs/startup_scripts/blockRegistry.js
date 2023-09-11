466a33d15ac103e71560c8d50d4635e6577e1839
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-12 01:21:09 +0200

    excitation coil added

---
+++ kubejs/startup_scripts/blockRegistry.js
@@ -0,0 +1,10 @@
+StartupEvents.registry("block", (event) => {
+
+    event.create('excitationcoil', 'cardinal').displayName("Excitation Coil").material("metals").renderType('cutout').box(3, 0, 3, 13, 1, 13)
+
+
+
+
+
+
+})
