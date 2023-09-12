31853bf878abaf94512f1341f8c6d709e4b0dc0d
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-12 02:13:03 +0200

    Remove extra newlines

--- kubejs/startup_scripts/blockRegistry.js
+++ kubejs/startup_scripts/blockRegistry.js
@@ -3,8 +3,4 @@ StartupEvents.registry("block", (event) => {
     event.create('excitationcoil', 'cardinal').displayName("Excitation Coil").material("metals").renderType('cutout').box(3, 0, 3, 13, 1, 13)
 
 
-
-
-
-
 })
