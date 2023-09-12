00ec83590d14505212f00d5410b3a7d9eae86d54
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-12 21:06:20 +0200

    Ported missing Thermal Stuff
    -Updated Mods
    -Added the Hand Framing Tool Item

--- kubejs/startup_scripts/blockRegistry.js
+++ kubejs/startup_scripts/blockRegistry.js
@@ -1,6 +1,8 @@
 StartupEvents.registry("block", (event) => {
 
-    event.create('excitationcoil', 'cardinal').displayName("Excitation Coil").material("metals").renderType('cutout').box(3, 0, 3, 13, 1, 13)
+    event.create('excitationcoil', 'cardinal').displayName("Excitation Coil").material('metal').renderType('cutout').box(3, 0, 3, 13, 1, 13).tag("mineable/pickaxe").box(4, 1, 4, 12, 9, 12)
+    event.create('dust').material('sand').hardness(0.4).resistance(0.4).tag("mineable/shovel").displayName("Dust Block").property(BlockProperties.FALLING)
+
 
 
 })
