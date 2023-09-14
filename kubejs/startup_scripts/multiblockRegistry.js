f7a553374316efd7907243b226da51e6df576581
NemEzanevem <99277804+NemEzanevem@users.noreply.github.com>
2023-09-14 21:10:49 +0200

    Added Microverse Multiblocks & Fixed Elements

--- kubejs/startup_scripts/multiblockRegistry.js
+++ kubejs/startup_scripts/multiblockRegistry.js
@@ -1,3 +1,4 @@
+
 // Small Microverse Projector Recipe Type
 GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
     event.create('basic_microverse')
@@ -5,28 +6,104 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
         .setEUIO('in')
         .setMaxIOSize(4, 16, 1, 0)
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
-		.setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
-	    .setSound(GTSoundEntries.COOLING);
+        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.COOLING);
 })
 
-// Small Microverse
+// Advanced Microverse Projector Recipe Type
+GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
+    event.create('advanced_microverse')
+        .category('multiblock')
+        .setEUIO('in')
+        .setMaxIOSize(4, 16, 0, 0)
+        .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.COOLING);
+})
+
+// Advanced Microverse Project II Recipe Type
+GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
+    event.create('advanced_microverse_ii')
+        .category('multiblock')
+        .setEUIO('in')
+        .setMaxIOSize(8, 16, 0, 0)
+        .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.COOLING);
+})
+
+// Basic Microverse Projector
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('basic_microverse_projector', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
+        .recipeType('basic_microverse')
         .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
         .pattern(definition => FactoryBlockPattern.start()
             .aisle("CMC", "CVC", "CCC")
             .aisle("CCC", "GDG", "CCC")
             .aisle("CSC", "CGC", "CCC")
-            .where('S', Predicates.controller(Predicates.blocks(definition.get()))
+            .where('S', Predicates.controller(Predicates.blocks(definition.get())))
             .where('D', Predicates.blocks("minecraft:diamond_block"))
-            .where('C', Predicates.blocks("gregtech:microverse_casing"))
-            .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()).setExactLimit(3))
-            .where('M', Predicates.abilities(PartAbility.MUFFLER).setExactLimit(1))
-            .where('V', Predicates.abilities(GTBlocks.CASING_GRATE.get())).setExactLimit(1))
-            .or(Predicates.autoAbilities(definition.getRecipeType()))
-            .recipeType('basic_microverse')
+            .where('C', Predicates.blocks("kubejs:microverse_casing")
+                .or(Predicates.autoAbilities(definition.getRecipeType())))
+            .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
+            .where('M', Predicates.abilities(PartAbility.MUFFLER))
+            .where('V', Predicates.blocks(GTBlocks.CASING_GRATE.get()))
             .build())
-        .workableCasingRenderer("gtceu:block/microverse_casing",
+        .workableCasingRenderer("kubejs:block/microverse_casing",
             "gtceu:block/multiblock/implosion_compressor", false)
+
+})
+
+// Advanced Microverse Projector
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+    event.create('advanced_microverse_projector', 'multiblock')
+        .rotationState(RotationState.NON_Y_AXIS)
+        .recipeType('advanced_microverse')
+        .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
+        .pattern(definition => FactoryBlockPattern.start()
+            .aisle("CCCCC", "CGGGC", "CGGGC", "CGGGC", "CCCCC")
+            .aisle("CVCVC", "GDDDG", "GDDDG", "GDDDG", "CVCVC")
+            .aisle("CCCCC", "GDDDG", "GD#DG", "GDDDG", "CCCCC")
+            .aisle("CVCVC", "GDDDG", "GDDDG", "GDDDG", "CVCVC")
+            .aisle("CCSCC", "CGGGC", "CGGGC", "CGGGC", "CCCCC")
+            .where('S', Predicates.controller(Predicates.blocks(definition.get())))
+            .where('D', Predicates.blocks("minecraft:diamond_block"))
+            .where('C', Predicates.blocks("kubejs:microverse_casing")
+                .or(Predicates.autoAbilities(definition.getRecipeType())))
+            .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
+            .where('V', Predicates.blocks(GTBlocks.CASING_GRATE.get()))
+            .where('#', Predicates.air())
+            .build())
+        .workableCasingRenderer("kubejs:block/microverse_casing",
+            "kubejs:block/multiblock/basic_machine_smile", false)
+})
+
+// Advanced Microverse Projecctor II 
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+    event.create('advanced_microverse_projector_ii', 'multiblock')
+        .rotationState(RotationState.NON_Y_AXIS)
+        .recipeType('advanced_microverse_ii')
+        .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
+        .pattern(definition => FactoryBlockPattern.start()
+            .aisle("#########", "#########", "##CCCCC##", "##CVCVC##", "##CCCCC##", "##CVCVC##", "##CCCCC##", "#########", "#########")
+            .aisle("#########", "##CGGGC##", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "##CGGGC##", "#########")
+            .aisle("##CCCCC##", "#CDDDDDC#", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "#CDDDDDC#", "##CCCCC##")
+            .aisle("##CGGGC##", "#CDDDDDC#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#CDDDDDC#", "##CGGGC##")
+            .aisle("##SGGGC##", "#CDDDDDC#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#CDDDDDC#", "##CGGGC##")
+            .aisle("##CGGGC##", "#CDDDDDC#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#CDDDDDC#", "##CGGGC##")
+            .aisle("##CCCCC##", "#CDDDDDC#", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "#CDDDDDC#", "##CCCCC##")
+            .aisle("#########", "##CGGGC##", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "##CGGGC##", "#########")
+            .aisle("#########", "#########", "##CCCCC##", "##CVCVC##", "##CCCCC##", "##CVCVC##", "##CCCCC##", "#########", "#########")
+            .where('S', Predicates.controller(Predicates.blocks(definition.get())))
+            .where('D', Predicates.blocks("minecraft:diamond_block"))
+            .where('C', Predicates.blocks("kubejs:microverse_casing")
+                .or(Predicates.autoAbilities(definition.getRecipeType())))
+            .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
+            .where('V', Predicates.blocks(GTBlocks.CASING_TITANIUM_PIPE.get()))
+            .where('#', Predicates.air())
+            .build())
+        .workableCasingRenderer("kubejs:block/microverse_casing",
+            "gtceu:block/multiblock/implosion_compressor", false)
+
 })
