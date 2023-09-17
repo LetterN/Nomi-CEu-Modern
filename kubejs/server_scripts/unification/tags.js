2fe1bc34f1761ba61a5c6ac4467eb130bdd28880
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-17 11:46:30 +0200

    AVARITIA ??? Updated .gitignore

--- kubejs/server_scripts/unification/tags.js
+++ kubejs/server_scripts/unification/tags.js
@@ -1,5 +1,7 @@
 ServerEvents.tags('item', event => {
-    event.add('forge:ingots/neutronium', 'avaritia:neutron_ingot')
-    event.add('forge:nuggets/neutronium', 'avaritia:neutron_nugget')
+    event.add('forge:ingots/neutronium', 'avaritia:neutronium_ingot')
+    event.add('forge:nuggets/neutronium', 'avaritia:neutronium_nugget')
+    event.add('forge:storage_blocks/neutronium', 'avaritia:neutronium_block')
+    event.add('forge:dusts/tiny/neutronium', 'avaritia:pile_of_neutrons')
     event.add('ae2:all_fluix', 'gtceu:fluix_gem')
 })
