84f773339973f4330fa55e9c741812322fb33b7d
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-18 22:57:17 +0200

    updated mods

--- kubejs/client_scripts/hideJEI.js
+++ kubejs/client_scripts/hideJEI.js
@@ -2,8 +2,23 @@ REIEvents.hideItems(event => {
 
     event.hide(['ae2:vibration_chamber'])
 
+    //SO ANNOYING
 })
 
 REIEvents.removeCategories(event => {
     event.yeet('minecraft:plugins/tag')
+})
+
+REIEvents.groupEntries(event => {
+
+    //FIXME Im gonna cry
+    const useNbt = ['chiselsandbits:block_bit']
+
+    useNbt.forEach(id => {
+        const item = Item.of(id)
+        const { namespace, path } = Utils.id(item.id)
+        event.groupSameItem(`kubejs:rei_groups/${namespace}/${path}`, item.name, item)
+    })
+
+
 })
