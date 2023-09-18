bc3b986411e07e817ec6d864f8a368a08b4e6e5d
NemEzanevem <99277804+NemEzanevem@users.noreply.github.com>
2023-09-18 20:28:36 +0200

    Fixed Microverse and added Naquadah Reactor

--- kubejs/startup_scripts/multiblockRegistry.js
+++ kubejs/startup_scripts/multiblockRegistry.js
@@ -20,7 +20,7 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
         .setSound(GTSoundEntries.COOLING);
 })
 
-// Advanced Microverse Project II Recipe Type
+// Advanced Microverse Projector II Recipe Type
 GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
     event.create('advanced_microverse_ii')
         .category('multiblock')
@@ -31,11 +31,33 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
         .setSound(GTSoundEntries.COOLING);
 })
 
+// Naquadah Reactor I Recipe type
+GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
+    event.create('naquadah_reactor_i')
+        .category('multiblock')
+        .setEUIO('in')
+        .setMaxIOSize(1, 1, 0, 0)
+        .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.COOLING);
+})
+
+// Naqudah Reactor II Recipe Type
+GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
+    event.create('naquadah_reactor_ii')
+        .category('multiblock')
+        .setEUIO('in')
+        .setMaxIOSize(1, 1, 0, 0)
+        .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.COOLING);
+})
+
 // Basic Microverse Projector
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('basic_microverse_projector', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
-        .recipeType('basic_microverse')
+        .recipeTypes('basic_microverse')
         .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
         .pattern(definition => FactoryBlockPattern.start()
             .aisle("CMC", "CVC", "CCC")
@@ -44,7 +66,7 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
             .where('S', Predicates.controller(Predicates.blocks(definition.get())))
             .where('D', Predicates.blocks("kubejs:starry_diamond_block"))
             .where('C', Predicates.blocks("kubejs:microverse_casing")
-                .or(Predicates.autoAbilities(definition.getRecipeType())))
+                .or(Predicates.autoAbilities(definition.getRecipeTypes())))
             .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
             .where('M', Predicates.abilities(PartAbility.MUFFLER))
             .where('V', Predicates.blocks(GTBlocks.CASING_GRATE.get()))
@@ -58,7 +80,7 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('advanced_microverse_projector', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
-        .recipeType('advanced_microverse')
+        .recipeTypes('advanced_microverse')
         .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
         .pattern(definition => FactoryBlockPattern.start()
             .aisle("CCCCC", "CGGGC", "CGGGC", "CGGGC", "CCCCC")
@@ -69,7 +91,7 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
             .where('S', Predicates.controller(Predicates.blocks(definition.get())))
             .where('D', Predicates.blocks("kubejs:starry_diamond_block"))
             .where('C', Predicates.blocks("kubejs:microverse_casing")
-                .or(Predicates.autoAbilities(definition.getRecipeType())))
+                .or(Predicates.autoAbilities(definition.getRecipeTypes())))
             .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
             .where('V', Predicates.blocks(GTBlocks.CASING_GRATE.get()))
             .where('#', Predicates.air())
@@ -82,22 +104,22 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('advanced_microverse_projector_ii', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
-        .recipeType('advanced_microverse_ii')
+        .recipeTypes('advanced_microverse_ii')
         .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
         .pattern(definition => FactoryBlockPattern.start()
             .aisle("#########", "#########", "##CCCCC##", "##CVCVC##", "##CCCCC##", "##CVCVC##", "##CCCCC##", "#########", "#########")
             .aisle("#########", "##CGGGC##", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "##CGGGC##", "#########")
             .aisle("##CCCCC##", "#CDDDDDC#", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "#CDDDDDC#", "##CCCCC##")
-            .aisle("##CGGGC##", "#CDDDDDC#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#CDDDDDC#", "##CGGGC##")
-            .aisle("##SGGGC##", "#CDDDDDC#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#CDDDDDC#", "##CGGGC##")
-            .aisle("##CGGGC##", "#CDDDDDC#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#CDDDDDC#", "##CGGGC##")
+            .aisle("##CGGGC##", "#GDDDDDG#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#GDDDDDG#", "##CGGGC##")
+            .aisle("##CGGGC##", "#GDDDDDG#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#GDDDDDG#", "##CGGGC##")
+            .aisle("##CGGGC##", "#GDDDDDG#", "CDDDDDDDC", "GDD###DDG", "GDD###DDG", "GDD###DDG", "CDDDDDDDC", "#GDDDDDG#", "##CGGGC##")
             .aisle("##CCCCC##", "#CDDDDDC#", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "CDDDDDDDC", "#CDDDDDC#", "##CCCCC##")
             .aisle("#########", "##CGGGC##", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "#CDDDDDC#", "##CGGGC##", "#########")
-            .aisle("#########", "#########", "##CCCCC##", "##CVCVC##", "##CCCCC##", "##CVCVC##", "##CCCCC##", "#########", "#########")
+            .aisle("#########", "#########", "##CCSCC##", "##CGGGC##", "##CGGGC##", "##CGGGC##", "##CCCCC##", "#########", "#########")
             .where('S', Predicates.controller(Predicates.blocks(definition.get())))
             .where('D', Predicates.blocks("kubejs:starry_diamond_block"))
             .where('C', Predicates.blocks("kubejs:microverse_casing")
-                .or(Predicates.autoAbilities(definition.getRecipeType())))
+                .or(Predicates.autoAbilities(definition.getRecipeTypes())))
             .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
             .where('V', Predicates.blocks(GTBlocks.CASING_TITANIUM_PIPE.get()))
             .where('#', Predicates.air())
@@ -105,4 +127,47 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
         .workableCasingRenderer("kubejs:block/microverse_casing",
             "gtceu:block/multiblock/implosion_compressor", false)
 
+})
+
+// Naquadah Reactor I
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+    event.create('naquadah_reactor_i', 'multiblock')
+        .rotationState(RotationState.NON_Y_AXIS)
+        .recipeTypes('naquadah_reactor_i')
+        .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
+        .pattern(definition => FactoryBlockPattern.start()
+            .aisle("CCC", "PGP", "PGP", "PGP", "CCC")
+            .aisle("CCC", "GNG", "GNG", "GOG", "CCC")
+            .aisle("CSC", "PGP", "PGP", "PGP", "CCC")
+            .where('S', Predicates.controller(Predicates.blocks(definition.get())))
+            .where('G', Predicates.blocks(GTBlocks.FUSION_GLASS.get()))
+            .where('C', Predicates.blocks("gtceu:reaction_safe_mixing_casing")
+                .or(Predicates.autoAbilities(definition.getRecipeTypes())))
+            .where('P', Predicates.blocks("ae2:spatial_pylon"))
+            .where('N', Predicates.blocks("gtceu:duranium_block"))
+            .where('O', Predicates.blocks("gtceu:black_steel_block"))
+            .build())
+        .workableCasingRenderer("gtceu:block/casings/gcym/reaction_safe_mixing_casing",
+            "gtceu:block/multiblock/implosion_compressor", false)
+})
+
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+    event.create('naquadah_reactor_ii', 'multiblock')
+        .rotationState(RotationState.NON_Y_AXIS)
+        .recipeTypes('naquadah_reactor_ii')
+        .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
+        .pattern(definition => FactoryBlockPattern.start()
+            .aisle("CCC", "PGP", "PGP", "PGP", "PGP", "CCC")
+            .aisle("CCC", "GNG", "GNG", "GNG", "GOG", "CCC")
+            .aisle("CSC", "PGP", "PGP", "PGP", "PGP", "CCC")
+            .where('S', Predicates.controller(Predicates.blocks(definition.get())))
+            .where('G', Predicates.blocks(GTBlocks.FUSION_GLASS.get()))
+            .where('C', Predicates.blocks("gtceu:reaction_safe_mixing_casing")
+                .or(Predicates.autoAbilities(definition.getRecipeTypes())))
+            .where('P', Predicates.blocks("ae2:spatial_pylon"))
+            .where('N', Predicates.blocks("gtceu:taranium_block"))
+            .where('O', Predicates.blocks("extendedcrafting:the_ultimate_block"))
+            .build())
+        .workableCasingRenderer("gtceu:block/casings/gcym/reaction_safe_mixing_casing",
+            "gtceu:block/multiblock/implosion_compressor", false)
 })
