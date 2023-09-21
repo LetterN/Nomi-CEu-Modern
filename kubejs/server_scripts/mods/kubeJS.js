1ba68e82abb94223223ae13d67bc5862066a4570
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-21 14:03:06 +0200

    updated Hostile Neural Networks

--- kubejs/server_scripts/mods/kubeJS.js
+++ kubejs/server_scripts/mods/kubeJS.js
@@ -4,15 +4,12 @@ ServerEvents.recipes(event => {
 
     //einsteinium
 
-    //FIXME Fusion Recipes
-    ServerEvents.recipes(event => {
-        event.recipes.gtceu.fusion_reactor('einsteinium_fusion')
-            .inputFluids(Fluid.of('gtceu:berkelium', 16), Fluid.of('gtceu:californium', 16))
-            .outputFluids(Fluid.of('gtceu:einsteinium', 16))
-            .duration(100)
-            .EUt(15360)
-            .inputEU(400000000)
-    })
+    //FIXME Fusion Recipes Energy to Start
 
+    event.recipes.gtceu.fusion_reactor('einsteinium_fusion')
+        .inputFluids(Fluid.of('gtceu:berkelium', 16), Fluid.of('gtceu:californium', 16))
+        .outputFluids(Fluid.of('gtceu:einsteinium', 16))
+        .duration(100)
+        .EUt(15360)
 
 })
