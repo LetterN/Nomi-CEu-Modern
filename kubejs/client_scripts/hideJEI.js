4d9fefa255e03fa3c2970cf0368376318b68639b
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-23 16:12:33 +0200

    Everything decided to work again..

--- kubejs/client_scripts/hideJEI.js
+++ kubejs/client_scripts/hideJEI.js
@@ -1,13 +1,16 @@
-REIEvents.hideItems(event => {
+REIEvents.hide('item', event => {
     //Hides useless items
     event.hide(['ae2:vibration_chamber', 'hammerlib:gears/netherite', 'hammerlib:gears/gold', 'hammerlib:gears/copper'])
 
+    //i really hate these kind of mods
+    event.hide('ironfurnaces:million_furnace')
+    event.hide(['ironfurnaces:item_spooky', 'ironfurnaces:item_xmas', 'ironfurnaces:item_copy', 'ironfurnaces:item_linker', 'ironfurnaces:rainbow_core', 'ironfurnaces:rainbow_plating', 'ironfurnaces:rainbow_coal', 'ironfurnaces:upgrade_iron', 'ironfurnaces:upgrade_gold', 'ironfurnaces:upgrade_diamond', 'ironfurnaces:upgrade_emerald', 'ironfurnaces:upgrade_obsidian', 'ironfurnaces:upgrade_crystal', 'ironfurnaces:upgrade_netherite', 'ironfurnaces:upgrade_copper', 'ironfurnaces:upgrade_obsidian2', 'ironfurnaces:upgrade_silver', 'ironfurnaces:upgrade_obsidian2', 'ironfurnaces:upgrade_iron2', 'ironfurnaces:upgrade_gold2', 'ironfurnaces:upgrade_silver2', 'ironfurnaces:allthemodium_furnace', 'ironfurnaces:vibranium_furnace', 'ironfurnaces:unobtainium_furnace', 'ironfurnaces:upgrade_allthemodium', 'ironfurnaces:upgrade_vibranium', 'ironfurnaces:upgrade_unobtainium', 'ironfurnaces:item_heater', 'ironfurnaces:augment_blasting', 'ironfurnaces:augment_smoking', 'ironfurnaces:heater', 'ironfurnaces:augment_generator'])
 
 })
 
 REIEvents.removeCategories(event => {
     //FIXME HELP
-    event.yeet('minecraft:plugins/tag')
+    event.remove('minecraft:plugins/tag')
 })
 
 REIEvents.groupEntries(event => {
@@ -24,25 +27,3 @@ REIEvents.groupEntries(event => {
 })
 
 //I hate compressium
-
-
-REIEvents.hideItems(event => {
-
-
-    const baseElementNames = ['compressium:granite_']
-
-
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-    for (const baseElementName of baseElementNames) {
-
-        for (const number of numbers) {
-            const elementName = `${baseElementName}${number}`
-
-            event.hide(elementName)
-        }
-    }
-
-
-})
