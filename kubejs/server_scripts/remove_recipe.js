de2460df35370ffa482fe386d4196000472880c1
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-09-23 16:12:33 +0200

    Everything decided to work again..

--- kubejs/server_scripts/remove_recipe.js
+++ kubejs/server_scripts/remove_recipe.js
@@ -3,4 +3,5 @@ ServerEvents.recipes(event => {
     event.remove({ output: ['ae2:vibration_chamber', 'hammerlib:gears/copper'] })
     event.remove({ id: 'avaritia:neutronium_nugget' })
 
+    //furnace stuff
 })
