90d32673c4a768172befc92c652f9cda27c6a9f6
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-10-25 18:30:14 +0200

    GCYR RAWRRR

--- kubejs/server_scripts/mods/kubeJS.js
+++ kubejs/server_scripts/mods/kubeJS.js
@@ -198,7 +198,7 @@ ServerEvents.recipes(event => {
     //Space Stuff
 
     event.recipes.gtceu.macerator('moon_dust')
-        .itemInputs('gcys:moon_stone')
+        .itemInputs('gcyr:moon_stone')
         .itemOutputs('kubejs:moon_dust')
         .duration(200)
         .EUt(400)
