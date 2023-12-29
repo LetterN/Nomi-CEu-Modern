454d6b4da67e2a30920d5d3cf0eca17165d0991b
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-12-29 21:44:54 +0100

    Bit of work on Thermal -Removed Lyeo chan

--- kubejs/server_scripts/mods/Thermal.js
+++ kubejs/server_scripts/mods/Thermal.js
@@ -1,7 +1,7 @@
 //TODO: Take care of all Thermal stuff
 
 ServerEvents.recipes(event => {
-    event.remove({ output: 'systeams:boiler_pipe' })
+    event.remove({ output: ['systeams:boiler_pipe', 'thermal:dynamo_fuel_augment', 'thermal:rf_coil'] })
     event.shaped(
         'systeams:boiler_pipe', [
             ' C ',
@@ -12,6 +12,29 @@ ServerEvents.recipes(event => {
             B: 'minecraft:bucket',
             C: 'gtceu:iron_gear',
             D: '#enderio:fused_quartz'
+        }
+    )
+
+    event.shaped(
+        'thermal:dynamo_fuel_augment', [
+            'ABA',
+            'BCB',
+            'ABA'
+        ], {
+            A: 'gtceu:wrought_iron_ingot',
+            B: 'gtceu:wrought_iron_plate',
+            C: 'thermal:rf_coil'
+        }
+    )
+
+    event.shaped(
+        'thermal:rf_coil', [
+            ' BA',
+            'BAB',
+            'AB '
+        ], {
+            A: 'gtceu:gold_rod',
+            B: 'minecraft:redstone'
         }
     )
 
