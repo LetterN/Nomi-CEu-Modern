5b9011e117d8c6fa62a3f9fb22e3df2efbbc688a
Reclipse <narcisprecup233@gmail.com>
2023-12-29 16:20:29 +0200

    Ore work - W.I.P.

---
+++ kubejs/server_scripts/gregtech/ore_veins.js
@@ -0,0 +1,26 @@
+GTCEuServerEvents.oreVeins(event => {
+    /*
+    GTCEuServerEvents.oreVeins(event => {
+    event.add("kubejs:custom_vein", vein => {
+        // Basic vein generation properties
+        vein.weight(200) // [*] 
+        vein.clusterSize(40) // [*] 
+        vein.density(0.25) // [*] 
+        vein.discardChanceOnAirExposure(0) // 
+
+        // Define where the vein can generate
+        vein.layer("deepslate") // [*] 
+        vein.dimensions("minecraft:overworld") // 
+        vein.biomes("#minecraft:is_overworld") // 
+
+        // Define a height range:
+        // You must choose EXACTLY ONE of these options! [*]
+        vein.heightRangeUniform(-60, 20)
+        // Define the vein's generator:
+        vein.generator(/* ... *//*) // [*] 
+        // Add one or more type of surface indicator to the vein:
+        vein.addIndicator(/* ... *//*) 
+        })
+    })
+    */
+})
