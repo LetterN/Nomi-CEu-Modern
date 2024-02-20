4d2bf835d5185bb6a1ebabfe5522ea77586d1523
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-02-19 20:21:19 -0600

    Update temp.js

--- kubejs/server_scripts/temp.js
+++ kubejs/server_scripts/temp.js
@@ -2,7 +2,7 @@ ServerEvents.recipes(event => {
 
     // Need some kind of teleportation, this mod will do for now
     event.remove({ output: 'teleporters:ender_crystal' })
-    event.recipes.gtceu.autoclave("teleport_crystal")
+    event.recipes.gtceu.autoclave("teleporter_crystal")
         .itemInputs(["minecraft:chorus_flower"])
         .inputFluids("gtceu:vibrant_alloy 288")
         .itemOutputs('teleporters:ender_crystal')
