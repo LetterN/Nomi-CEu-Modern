d80bd79181c14d18da5f130b8d3eef671584c3c3
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-02-27 21:00:41 +0100

    Luminessence Recipes -updated mods -changed some creative recipes

--- kubejs/server_scripts/mods/extended_crafting.js
+++ kubejs/server_scripts/mods/extended_crafting.js
@@ -14,72 +14,72 @@ ServerEvents.recipes(event => {
             B: 'gtceu:iron_plate'
         })
 
-        event.remove({ output: 'extendedcrafting:advanced_component' })
-        event.shaped(
-            'extendedcrafting:advanced_component', [
+    event.remove({ output: 'extendedcrafting:advanced_component' })
+    event.shaped(
+        'extendedcrafting:advanced_component', [
+            'SL ',
+            'AB ',
+            '   '
+        ], {
+            S: 'gtceu:black_steel_plate',
+            L: 'extendedcrafting:luminessence',
+            A: 'gtceu:electrum_plate',
+            B: 'gtceu:glowstone_plate'
+        })
+
+    event.remove({ output: 'extendedcrafting:elite_component' })
+    event.shaped(
+        'extendedcrafting:elite_component', [
+            'SL ',
+            'AB ',
+            '   '
+        ], {
+            S: 'gtceu:black_steel_plate',
+            L: 'extendedcrafting:luminessence',
+            A: 'gtceu:aluminium_plate',
+            B: 'gtceu:enderpearl_plate'
+        })
+
+    event.remove({ output: 'extendedcrafting:ultimate_component' })
+    event.shaped(
+        'extendedcrafting:ultimate_component', [
+            'SL ',
+            'AB ',
+            '   '
+        ], {
+            S: 'gtceu:black_steel_plate',
+            L: 'extendedcrafting:luminessence',
+            A: 'gtceu:uranium_plate',
+            B: 'gtceu:emerald_plate'
+        })
+
+    event.remove({ output: 'extendedcrafting:crystaltine_component' })
+    event.shaped(
+        'extendedcrafting:crystaltine_component', [
+            'SL ',
+            'AB ',
+            '   '
+        ], {
+            S: 'gtceu:black_steel_plate',
+            L: 'extendedcrafting:luminessence',
+            A: 'gtceu:osmiridium_plate',
+            B: 'extendedcrafting:crystaltine_ingot'
+        })
+
+    event.remove({ output: 'extendedcrafting:redstone_component' })
+    event.shaped(
+            'extendedcrafting:redstone_component', [
                 'SL ',
                 'AB ',
                 '   '
             ], {
                 S: 'gtceu:black_steel_plate',
                 L: 'extendedcrafting:luminessence',
-                A: 'gtceu:electrum_plate',
-                B: 'gtceu:glowstone_plate'
+                A: 'gtceu:copper_plate',
+                B: 'gtceu:redstone_plate'
             })
-
-            event.remove({ output: 'extendedcrafting:elite_component' })
-            event.shaped(
-                'extendedcrafting:elite_component', [
-                    'SL ',
-                    'AB ',
-                    '   '
-                ], {
-                    S: 'gtceu:black_steel_plate',
-                    L: 'extendedcrafting:luminessence',
-                    A: 'gtceu:aluminium_plate',
-                    B: 'gtceu:enderpearl_plate'
-                })
-    
-                event.remove({ output: 'extendedcrafting:ultimate_component' })
-                event.shaped(
-                    'extendedcrafting:ultimate_component', [
-                        'SL ',
-                        'AB ',
-                        '   '
-                    ], {
-                        S: 'gtceu:black_steel_plate',
-                        L: 'extendedcrafting:luminessence',
-                        A: 'gtceu:uranium_plate',
-                        B: 'gtceu:emerald_plate'
-                    })
-        
-                    event.remove({ output: 'extendedcrafting:crystaltine_component' })
-                    event.shaped(
-                        'extendedcrafting:crystaltine_component', [
-                            'SL ',
-                            'AB ',
-                            '   '
-                        ], {
-                            S: 'gtceu:black_steel_plate',
-                            L: 'extendedcrafting:luminessence',
-                            A: 'gtceu:osmiridium_plate',
-                            B: 'extendedcrafting:crystaltine_ingot'
-                        })
-
-                        event.remove({ output: 'extendedcrafting:redstone_component' })
-                        event.shaped(
-                            'extendedcrafting:redstone_component', [
-                                'SL ',
-                                'AB ',
-                                '   '
-                            ], {
-                                S: 'gtceu:black_steel_plate',
-                                L: 'extendedcrafting:luminessence',
-                                A: 'gtceu:copper_plate',
-                                B: 'gtceu:redstone_plate'
-                            })
-    //Catalysts
-	event.replaceInput({ id: /extendedcrafting/ }, 'extendedcrafting:black_iron_ingot', 'gtceu:black_steel_plate')
+        //Catalysts
+    event.replaceInput({ id: /extendedcrafting/ }, 'extendedcrafting:black_iron_ingot', 'gtceu:black_steel_plate')
 
     event.shaped(
         'kubejs:conflux_catalyst', [
@@ -110,87 +110,104 @@ ServerEvents.recipes(event => {
             D: 'extendedcrafting:advanced_catalyst'
         })
 
-        event.remove({ output: 'extendedcrafting:elite_table' })
-        event.recipes.extendedcrafting.shaped_table(
-            'extendedcrafting:elite_table', [
-                'ASESA',
-                'SCCCS',
-                'ECTCE',
-                'SCCCS',
-                'ASESA'
-            ], {
-                A: 'gtceu:aluminium_block',
-                S: 'gtceu:black_steel_plate',
-                E: 'extendedcrafting:elite_catalyst',
-                C: 'extendedcrafting:elite_component',
-                T: 'extendedcrafting:advanced_table'
-            }, 2
-        )
-        event.remove({ output: 'extendedcrafting:ultimate_table' })
-        event.recipes.extendedcrafting.shaped_table(
-                'extendedcrafting:ultimate_table', [
-                    'ESSCSSE',
-                    'SUUUUUS',
-                    'SUSRSUS',
-                    'CURTRUC',
-                    'SUSRSUS',
-                    'SUUUUUS',
-                    'ESSCSSE'
-                ], {
-                    E: 'minecraft:emerald_block',
-                    S: 'gtceu:black_steel_plate',
-                    C: 'kubejs:conflux_catalyst',
-                    U: 'extendedcrafting:ultimate_component',
-                    R: 'extendedcrafting:crystaltine_component',
-                    T: 'extendedcrafting:elite_table'
-                }, 3
-        )
+    event.remove({ output: 'extendedcrafting:elite_table' })
+    event.recipes.extendedcrafting.shaped_table(
+        'extendedcrafting:elite_table', [
+            'ASESA',
+            'SCCCS',
+            'ECTCE',
+            'SCCCS',
+            'ASESA'
+        ], {
+            A: 'gtceu:aluminium_block',
+            S: 'gtceu:black_steel_plate',
+            E: 'extendedcrafting:elite_catalyst',
+            C: 'extendedcrafting:elite_component',
+            T: 'extendedcrafting:advanced_table'
+        }, 2
+    )
+    event.remove({ output: 'extendedcrafting:ultimate_table' })
+    event.recipes.extendedcrafting.shaped_table(
+        'extendedcrafting:ultimate_table', [
+            'ESSCSSE',
+            'SUUUUUS',
+            'SUSRSUS',
+            'CURTRUC',
+            'SUSRSUS',
+            'SUUUUUS',
+            'ESSCSSE'
+        ], {
+            E: 'minecraft:emerald_block',
+            S: 'gtceu:black_steel_plate',
+            C: 'kubejs:conflux_catalyst',
+            U: 'extendedcrafting:ultimate_component',
+            R: 'extendedcrafting:crystaltine_component',
+            T: 'extendedcrafting:elite_table'
+        }, 3
+    )
 
-//        let tables = ['extendedcrafting:advanced_table', 'extendedcrafting:elite_table', 'extendedcrafting:ultimate_table']
-//        tables.forEach(table => {
-//            event.shapeless(2x table, [table])
-//        });
-        
+    //        let tables = ['extendedcrafting:advanced_table', 'extendedcrafting:elite_table', 'extendedcrafting:ultimate_table']
+    //        tables.forEach(table => {
+    //            event.shapeless(2x table, [table])
+    //        });
+
     // Combination Crafts
     event.recipes.extendedcrafting.combination(
         "gtceu:restonia_empowered_block",
-        "gtceu:restonia_block",
-        ["gtceu:red_alloy_ingot", "gtceu:almandine_gem", Item.of('gtceu:glass_vial', '{Fluid:{Amount:1000,FluidName:"gtceu:sulfuric_acid"}}'), "extendedcrafting:the_ultimate_component"],
+        "gtceu:restonia_block", ["gtceu:red_alloy_ingot", "gtceu:almandine_gem", Item.of('gtceu:glass_vial', '{Fluid:{Amount:1000,FluidName:"gtceu:sulfuric_acid"}}'), "extendedcrafting:the_ultimate_component"],
         4000000, 400000
     )
 
     event.recipes.extendedcrafting.combination(
         "gtceu:diamatine_empowered_block",
-        "gtceu:diamatine_block",
-        ["extendedcrafting:ender_star", "extendedcrafting:crystaltine_ingot", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"gtceu:nitrogen_dioxide"}}'), "extendedcrafting:crystaltine_component"],
+        "gtceu:diamatine_block", ["extendedcrafting:ender_star", "extendedcrafting:crystaltine_ingot", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"gtceu:nitrogen_dioxide"}}'), "extendedcrafting:crystaltine_component"],
         4000000, 400000
     )
 
     event.recipes.extendedcrafting.combination(
         "gtceu:emeradic_empowered_block",
-        "gtceu:emeradic_block",
-        ["kubejs:stabilized_uranium", "gtceu:vibrant_alloy_ingot", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"gtceu:argon"}}').strongNBT(), "extendedcrafting:ultimate_component"],
+        "gtceu:emeradic_block", ["kubejs:stabilized_uranium", "gtceu:vibrant_alloy_ingot", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"gtceu:argon"}}').strongNBT(), "extendedcrafting:ultimate_component"],
         4000000, 400000
     )
 
     event.recipes.extendedcrafting.combination(
         "gtceu:enori_empowered_block",
-        "gtceu:enori_block",
-        ["gtceu:apatite_gem", "gtceu:end_steel_ingot", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"gtceu:helium"}}').strongNBT(), "extendedcrafting:basic_component"],
+        "gtceu:enori_block", ["gtceu:apatite_gem", "gtceu:end_steel_ingot", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"gtceu:helium"}}').strongNBT(), "extendedcrafting:basic_component"],
         4000000, 400000
     )
 
     event.recipes.extendedcrafting.combination(
         "gtceu:palis_empowered_block",
-        "gtceu:palis_block",
-        ["gtceu:sapphire_gem", "minecraft:bedrock", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"minecraft:water"}}').strongNBT(), "extendedcrafting:elite_component"],
+        "gtceu:palis_block", ["gtceu:sapphire_gem", "minecraft:bedrock", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"minecraft:water"}}').strongNBT(), "extendedcrafting:elite_component"],
         4000000, 400000
     )
 
     event.recipes.extendedcrafting.combination(
         "gtceu:void_empowered_block",
-        "gtceu:void_block",
-        ["gtceu:black_quartz_gem", "gtceu:dark_steel_ingot", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"gtceu:oil"}}').strongNBT(), "extendedcrafting:advanced_component"],
+        "gtceu:void_block", ["gtceu:black_quartz_gem", "gtceu:dark_steel_ingot", Item.of('gtceu:fluid_cell', '{Fluid:{Amount:1000,FluidName:"gtceu:oil"}}').strongNBT(), "extendedcrafting:advanced_component"],
         4000000, 400000
     )
+
+    //Luminessence
+    event.remove({ id: 'extendedcrafting:luminessence' })
+
+    event.recipes.gtceu.chemical_reactor('phosphoric_acid_apatite')
+        .itemInputs('9x gtceu:apatite_dust')
+        .inputFluids('water 10000', 'gtceu:sulfuric_acid 5000')
+        .itemOutputs('40x gtceu:gypsum_dust')
+        .outputFluids('gtceu:hydrochloric_acid 1000', 'gtceu:phosphoric_acid 3000')
+        .duration(320)
+        .EUt(30)
+
+    event.recipes.gtceu.mixer('luminessence')
+        .itemInputs('minecraft:redstone', 'minecraft:glowstone_dust', '2x gtceu:aluminium_dust')
+        .inputFluids('gtceu:phosphoric_acid 4000')
+        .itemOutputs('8x extendedcrafting:luminessence')
+        .duration(20)
+        .EUt(30)
+
+
+
+
+
 })
