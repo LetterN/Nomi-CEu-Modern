d7513e72cc8b79cad7752676b5d42f538321c684
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-02-27 21:23:25 -0600

    Closes #32

--- kubejs/client_scripts/capes.js
+++ kubejs/client_scripts/capes.js
@@ -1,3 +1,4 @@
 CapeJS.addCapes(event => {
+    event.register("e8e08277-cc3d-4b35-8ac9-da0af044815c", "gregtechcape")
     event.register("00832ce6-e198-4bc0-8597-aa99c4133a38", "gregtechcape")
 })
