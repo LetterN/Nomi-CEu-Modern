60286ab326663d050e1b9695d55067fd0c0ea4d7
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-02-28 12:40:07 -0600

    Closes #33

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -42,4 +42,9 @@ ItemEvents.tooltip(tooltip => {
     tooltip.add('enderio:draconic_superconductor_conduit', '\u00A77Max Output 134,217,728 RF/t')
 
     tooltip.add("thermal:device_rock_gen", "\u00A77Must be placed next to lava and water to produce cobblestone.")
+
+    // Backpacks, this extra bit of code will made it so that this line will be the first line displayed after the item name (if curios didn't exist lol)
+    tooltip.addAdvanced(/simplybackpacks:/, (item, advanced, text) => {
+        text.add(1, [Text.of('Items in backpack will be lost if upgraded!').red()])
+      })
 })
