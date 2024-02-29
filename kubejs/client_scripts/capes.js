061c362a6468a933306a9b872faed241449ceb39
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-02-29 15:19:38 -0600

    Update capes.js

--- kubejs/client_scripts/capes.js
+++ kubejs/client_scripts/capes.js
@@ -1,3 +1,6 @@
 CapeJS.addCapes(event => {
+    event.register("e8e08277-cc3d-4b35-8ac9-da0af044815c", "gregtechcape")
+    event.register("dd0baab4-8874-4249-912a-44e64f7b1786", "gregtechcape")
+    event.register("dd0baab4-8874-4249-912a-44e64f7b1786", "gregtechcape")
     event.register("00832ce6-e198-4bc0-8597-aa99c4133a38", "gregtechcape")
 })
