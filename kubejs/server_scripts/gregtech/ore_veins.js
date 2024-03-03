7284aafe481ff7d23442d742162e75028eb7a095
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-03-03 17:24:43 -0600

    overworld tetrahedrite vein

--- kubejs/server_scripts/gregtech/ore_veins.js
+++ kubejs/server_scripts/gregtech/ore_veins.js
@@ -39,6 +39,24 @@ GTCEuServerEvents.oreVeins(event => {
             )
         )
     })
+
+    event.add("kubejs:overworltetrahedrite", vein => {
+        vein.weight(30)
+        vein.clusterSize(30)    
+        vein.density(0.25)
+        vein.discardChanceOnAirExposure(0)
+        vein.layer("stone")
+        vein.dimensions("minecraft:overworld")
+        vein.biomes("#minecraft:is_overworld")
+        vein.heightRangeUniform(60, 120)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(2).mat(GTMaterials.Tetrahedrite).size(1, 1))
+                .layer(l => l.weight(2).mat(GTMaterials.Copper).size(2, 4))
+                .layer(l => l.weight(1).mat(GTMaterials.Stibnite).size(1, 4))
+            )
+        )
+    })
 })
 
 
