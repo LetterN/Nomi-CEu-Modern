6e7803c784ed77e80c0cb395958325bdd9b522b0
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2024-03-06 16:15:18 -0600

    Clean up placement script and add comments

--- kubejs/server_scripts/better_placement.js
+++ kubejs/server_scripts/better_placement.js
@@ -15,9 +15,19 @@ function opposite(face) {
 
 BlockEvents.placed(event => {
 	let block = event.getBlock()
+
+	//gtceu blocks
+	if (block.getId().startsWith('gtceu:')) {
+		//Set energy converters to feToEu mode when placed
+		if (block.getId().endsWith('_energy_converter')) {
+			block.mergeEntityData({energyContainer: {feToEu: true}})
+		}
+	}
+
+	//Blocks below these line only get their placements altered if they were placed by an entity
+	if (event.getEntity() == null) return
+
 	// Reverse placed Dynamos on Sneak 
-	if (event.getEntity() == null)
-		return
 	if (block.getId().startsWith('thermal:dynamo') || (block.getId().startsWith('systeams:'))) { //systeams:steam is not a block thankfully
 		let properties = block.getProperties()
 		if (event.getEntity().isCrouching()) {
@@ -26,9 +36,4 @@ BlockEvents.placed(event => {
 		}
 	}
 
-	if (block.getId().startsWith('gtceu:')) {
-		if (block.getId().endsWith('_energy_converter')) {
-			block.mergeEntityData({energyContainer: {feToEu: true}})
-		}
-	}
 })
