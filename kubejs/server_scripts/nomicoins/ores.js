267a4571cc9be9b946d26789c5593245af1972f8
G'lek Tarssza <glek@glektarssza.com>
2024-03-20 05:18:25 -0600

    Broke seeds into their own file.

--- kubejs/server_scripts/nomicoins/ores.js
+++ kubejs/server_scripts/nomicoins/ores.js
@@ -11,42 +11,6 @@ ServerEvents.recipes(event => {
     }).noMirror().noShrink()
 
     event.shaped(
-        Item.of('minecraft:cocoa_beans', 32), [
-        'AA ',
-        ' A ',
-        '   '
-    ], {
-        A: 'kubejs:nomi_penny'
-    }).noMirror().noShrink()
-
-    event.shaped(
-        Item.of('minecraft:sugar_cane', 32), [
-        'A A',
-        '   ',
-        '   '
-    ], {
-        A: 'kubejs:nomi_penny'
-    }).noMirror().noShrink()
-
-    event.shaped(
-        Item.of('minecraft:cactus', 32), [
-        'A  ',
-        'A  ',
-        '   '
-    ], {
-        A: 'kubejs:nomi_penny'
-    }).noMirror().noShrink()
-
-    event.shaped(
-        Item.of('minecraft:potato', 32), [
-        'AA ',
-        '   ',
-        '   '
-    ], {
-        A: 'kubejs:nomi_penny'
-    }).noMirror().noShrink()
-
-    event.shaped(
         Item.of('gtceu:pitchblende_ore', 32), [
         'A  ',
         'AAA',
