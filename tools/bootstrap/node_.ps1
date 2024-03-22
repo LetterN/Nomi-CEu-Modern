1e2a3689bef24c80a7b47f0ae206fc17484477b7
LetterN <24603524+LetterN@users.noreply.github.com>
2024-03-22 23:21:37 +0800

    bump node to v18

--- tools/bootstrap/node_.ps1
+++ tools/bootstrap/node_.ps1
@@ -30,7 +30,7 @@ $Cache = "$BaseDir\.cache"
 if ($Env:TG_BOOTSTRAP_CACHE) {
 	$Cache = $Env:TG_BOOTSTRAP_CACHE
 }
-$NodeVersion = "16.7.0"
+$NodeVersion = "18.19.1"
 $NodeSource = "https://nodejs.org/download/release/v$NodeVersion/win-x64/node.exe"
 $NodeTargetDir = "$Cache\node-v$NodeVersion-x64"
 $NodeTarget = "$NodeTargetDir\node.exe"
