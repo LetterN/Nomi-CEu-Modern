5af583f7ae46ca934f64166082cb6159ad187ab7
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-03-22 19:34:45 +0100

    Make dynamo able to be broken

--- kubejs/server_scripts/Block_Events.js
+++ kubejs/server_scripts/Block_Events.js
@@ -1,8 +1,16 @@
     // For some unholy reason watercondenser refuses to work with loottables, so I gotta fight jank with jank
-    BlockEvents.broken(event =>{
+    BlockEvents.broken(event => {
         if (event.block.id != 'watercondenser:watercondenser') return
         let wc = event.block.createEntity('item')
         wc.item = Item.of('watercondenser:watercondenser')
+        wc.item.count = 1
+        wc.spawn()
+    })
+
+    BlockEvents.broken(event => {
+        if (event.block.id != 'steamdynamo:steam_dynamo') return
+        let wc = event.block.createEntity('item')
+        wc.item = Item.of('steamdynamo:steam_dynamo')
         wc.item.count = 1
         wc.spawn()
     })
--- kubejs/server_scripts/unification/tags.js
+++ kubejs/server_scripts/unification/tags.js
@@ -17,7 +17,7 @@ ServerEvents.tags('item', event => {
 })
 
 ServerEvents.tags('block', event => {
-    event.add('minecraft:mineable/pickaxe', 'watercondenser:watercondenser')
+    event.add('minecraft:mineable/pickaxe', 'steamdynamo:steam_dynamo')
     event.add('minecraft:mineable/pickaxe', 'forge:glass')
     event.add('minecraft:mineable/pickaxe', 'forge:glass_panes')
 })
