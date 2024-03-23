26cc51469c2d2517a8f1b6c52df2205b0a91bbd1
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-03-23 13:30:30 +0100

    Forgor draconic evolution

--- kubejs/client_scripts/DraconicEvolutionJEI.js
+++ kubejs/client_scripts/DraconicEvolutionJEI.js
@@ -6,7 +6,7 @@ JEIEvents.hideItems(event => {
 })
 
 //EMI Hacky Fix
-global.manualUnification = ['hammerlib:gears/stone', 'hammerlib:gears/wooden', 'gtceu:neutronium_ingot', 'gtceu:neutronium_nugget']
+global.manualUnification = ['draconicevolution:draconium_ingot', 'draconicevolution:draconium_nugget', 'draconicevolution:draconium_dust', 'draconicevolution:awakened_draconium_ingot', 'draconicevolution:awakened_draconium_nugget', 'draconicevolution:awakened_draconium_dust', 'hammerlib:gears/stone', 'hammerlib:gears/wooden', 'gtceu:neutronium_ingot', 'gtceu:neutronium_nugget']
 global.UnificationExcludedITems = ['nuclearcraft:hard_carbon_ingot', 'nuclearcraft:ferroboron_ingot', 'nuclearcraft:tough_alloy_ingot']
 global.unificationPattern = new RegExp(`^(?!${global.UnificationExcludedITems.join('|')}).*(nuclearcraft|thermal):(.*(_block|_plate|_ingot|_nugget|_gear))`, 'i')
 
