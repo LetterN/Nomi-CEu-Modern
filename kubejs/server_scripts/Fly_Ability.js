48d140a1ac67d5fe4e25f53c0a242cd593642f8d
Letter N <24603524+LetterN@users.noreply.github.com>
2024-03-29 15:20:33 +0800

    optimize fly ability (#244)

--- kubejs/server_scripts/Fly_Ability.js
+++ kubejs/server_scripts/Fly_Ability.js
@@ -1,20 +1,30 @@
-PlayerEvents.tick(event => {
-    const { player } = event;
 
-    if (event.player.age % 20 != 0) return
-
+/**
+ * @private
+ * @param {Internal.PlayerEventJS} ev
+ */
+const glitchArmorCallback = (ev) => {
+	const { player } = ev;
     const glitchArmorEquipped = player.headArmorItem === 'kubejs:glitch_infused_helmet' &&
         player.chestArmorItem === 'kubejs:glitch_infused_chestplate' &&
         player.legsArmorItem === 'kubejs:glitch_infused_leggings' &&
         player.feetArmorItem === 'kubejs:glitch_infused_boots'
 
-    if (glitchArmorEquipped || player.isCreative()) {
+    if (!player.abilities.mayfly && (glitchArmorEquipped || player.isCreative())) {
         player.abilities.mayfly = true
         player.onUpdateAbilities()
-    } else if (player.abilities.mayfly || !player.isCreative()) {
+		return;
+    }
+	if (player.abilities.mayfly && !glitchArmorEquipped && !player.isCreative()) {
         player.abilities.mayfly = false
         player.abilities.flying = false
         player.onUpdateAbilities()
     }
-
-})
+}
+// Notice: this wont update if you swap gamemode, you have to move glitch armor around your inventory!
+PlayerEvents.inventoryChanged('kubejs:glitch_infused_helmet', glitchArmorCallback);
+PlayerEvents.inventoryChanged('kubejs:glitch_infused_chestplate', glitchArmorCallback);
+PlayerEvents.inventoryChanged('kubejs:glitch_infused_leggings', glitchArmorCallback);
+PlayerEvents.inventoryChanged('kubejs:glitch_infused_boots', glitchArmorCallback);
+PlayerEvents.respawned(glitchArmorCallback); // if the armor has an enchant that keeps it
+PlayerEvents.loggedIn(glitchArmorCallback);
