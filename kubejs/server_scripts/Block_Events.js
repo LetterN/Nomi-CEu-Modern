654a38cc2b7ab4589a3b500fb5355a6a7e0a6ec0
Oier Bravo Urtasun <oierbravo@gmail.com>
2024-03-31 22:58:53 +0200

    Removed watercondeser loot and block event. Should be fixed in the mods last version (#271)

--- kubejs/server_scripts/Block_Events.js
+++
@@ -1,8 +0,0 @@
-// For some unholy reason watercondenser refuses to work with loottables, so I gotta fight jank with jank
-BlockEvents.broken(event => {
-    if (event.block.id != 'watercondenser:watercondenser') return
-    let wc = event.block.createEntity('item')
-    wc.item = Item.of('watercondenser:watercondenser')
-    wc.item.count = 1
-    wc.spawn()
-})
